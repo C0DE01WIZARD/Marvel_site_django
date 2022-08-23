4b5b99db0ab40622187fc444c2c1b4a691f960de
@Сode_SORCERER / Python/ HTML/ CSS <93222744+C0DE01WIZARD@users.noreply.github.com>
2022-08-24 00:16:58 +0300

    Update settings.py

--- Marvel/settings.py
+++ Marvel/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*^3%n41!)e%^_jbhg_%mj0igd_!mwgm3#k1z=uo10%7*0_7!m*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # отладка проекта
